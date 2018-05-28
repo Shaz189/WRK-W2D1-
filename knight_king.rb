ed4e52a1eb94c6176e595524e235204a51aaa7d7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-05-28 16:08:22 -0400

    initial commit

---
+++ knight_king.rb
@@ -0,0 +1,11 @@
+class Knight < Piece
+  def initialize
+    
+  end
+end
+
+class King < Piece
+  def initialize
+    
+  end
+end
---
+++ rook_bishop_queen.rb
@@ -0,0 +1,20 @@
+class Rook < Piece 
+  def initialize
+    
+  end
+  
+end
+
+class Bishop < Piece 
+  def initialize
+    
+  end
+  
+end
+
+  class Queen < Piece 
+  def initialize
+    
+  end
+  
+end
